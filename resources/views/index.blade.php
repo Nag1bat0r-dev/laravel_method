4a8e7f5583d40c24e5b9450797ebfbfdc5c4f32c
Nag1bat0r-dev <lakator08@gmail.com>
2025-03-14 07:18:56 +0500

    Updated form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,30 +6,30 @@
     <title>Form Methods</title>
 </head>
 <body>
-    <h1>Laravel Methods</h1>
+    <h1>Laravel Form Methods</h1>
 
 
-    <form action="/form" method="GET">
+    <form action="form.index" method="GET">
         <input type="text" name="name" placeholder="Ismingiz">
         <input type="text" name="email" placeholder="Email">
         <button type="submit">GET Request</button>
     </form>
 
-    <form action="/form" method="POST">
+    <form action="form.store" method="POST">
         @csrf
-        <input type="text" name="name" placeholder="Ismingizni tasdiqlang">
-        <input type="text" name="email" placeholder="Emailingizni tasdiqlang">
+        <input type="text" name="name" placeholder="Ismingiz">
+        <input type="text" name="email" placeholder="Email">
         <button type="submit">POST Request</button>
     </form>
 
-    <form action="/form" method="POST">
+    <form action="form.delete" method="POST">
         @csrf
         @method('DELETE')
-        <input type="text" name="id" placeholder="IDnigizni kiriting">
-        <button type="submit">DESTROY Request</button>
+        <input type="text" name="id" placeholder="ID ni kiriting">
+        <button type="submit">DELETE Request</button>
     </form>
 
-    <form action="/form" method="POST">
+    <form action="form.update" method="POST">
         @csrf
         @method('PUT')
         <input type="text" name="name" placeholder="Yangi ismingiz">
@@ -37,10 +37,10 @@
         <button type="submit">PUT Request</button>
     </form>
 
-    <form action="/form" method="POST">
+    <form action="form.patch" method="POST">
         @csrf
         @method('PATCH')
-        <input type="text" name="name" placeholder="Yangi ismingiz">
+        <input type="text" name="name" placeholder="Yangilangan ismingiz">
         <button type="submit">PATCH Request</button>
     </form>
 </body>
