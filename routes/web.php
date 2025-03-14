c09958dc2dd545d98b9649a443b87d8de319f6b3
Nag1bat0r-dev <lakator08@gmail.com>
2025-03-14 07:18:00 +0500

    Updated route

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
 
 
-Route::get('/form', [PostController::class, 'index']);
 
-Route::post('/form', [PostController::class, 'store']);
-Route::delete('/form', [PostController::class, 'delete']);
-Route::put('/form', [PostController::class, 'update']);
-Route::patch('/form', [PostController::class, 'patch']);
+Route::get('/form', [PostController::class, 'index'])->name('form.index');
+Route::post('/form', [PostController::class, 'store'])->name('form.store');
+Route::delete('/form', [PostController::class, 'delete'])->name('form.delete');
+Route::put('/form', [PostController::class, 'update'])->name('form.update');
+Route::patch('/form', [PostController::class, 'patch'])->name('form.patch');
